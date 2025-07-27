8de7109b71d61d3ae2c4e610a1aade77456acc6c
effie882 <effiemorara@gmail.com>
2025-07-27 21:44:49 +0300

    final

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,22 @@
-import React from "react";
+// src/components/ShoppingList.js
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const filteredItems = items.filter((item) => {
+    return selectedCategory === "All" || item.category === selectedCategory;
+  });
+
+  function handleChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
+      <div className="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,8 +24,12 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {filteredItems.map((item) => (
+          <Item
+            key={item.id}
+            name={item.name}
+            category={item.category}
+          />
         ))}
       </ul>
     </div>
--- src/data/items.js
+++ src/data/items.js
@@ -1,9 +1,11 @@
+// src/data/items.js
 const items = [
   { id: 1, name: "Yogurt", category: "Dairy" },
-  { id: 2, name: "Pomegranate", category: "Produce" },
-  { id: 3, name: "Lettuce", category: "Produce" },
-  { id: 4, name: "String Cheese", category: "Dairy" },
-  { id: 5, name: "Cookies", category: "Dessert" },
+  { id: 2, name: "Apple", category: "Produce" },
+  { id: 3, name: "Ice Cream", category: "Dessert" },
+  { id: 4, name: "Milk", category: "Dairy" },
+  { id: 5, name: "Carrots", category: "Produce" },
+  { id: 6, name: "Cookies", category: "Dessert" },
 ];
 
 export default items;
